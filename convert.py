c0719715d104777e0a9f78bd7376294ba7012c81
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-10 11:50:03 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,7 +1,5 @@
 import os
 import base64
-import json
-import yaml
 
 # 读取数据文件列表
 data_dir = "data"  # 替换为你的数据文件所在目录
@@ -9,34 +7,23 @@ data_files = os.listdir(data_dir)
 
 merged_content = set()
 
-# 遍历数据文件，合并内容并去重
+# 遍历数据文件，逐个检测内容并处理
 for file in data_files:
     file_path = os.path.join(data_dir, file)
     with open(file_path, "r") as f:
         content = f.read()
 
-        # 判断文件类型
-        if file.endswith(".json"):
-            # 跳过处理JSON文件
+        try:
+            # 尝试解密 Base64 编码的内容
+            decoded_content = base64.b64decode(content).decode()
+            merged_content.add(decoded_content)
+        except base64.binascii.Error:
+            # 文件内容不是 Base64 编码，跳过该文件
             continue
-        elif file.endswith(".yaml"):
-            # 跳过处理YAML文件
-            continue
-        elif file.endswith(".txt"):
-            # 处理文本文件
-            merged_content.add(content)
-        elif file.endswith(".base64"):
-            # 解密Base64编码的内容
-            try:
-                decoded_content = base64.b64decode(content).decode()
-                merged_content.add(decoded_content)
-            except base64.binascii.Error:
-                print(f"Error: Invalid Base64 encoding in file {file}")
-        else:
-            print(f"Warning: Unknown file type for file {file}")
 
 # 输出合并且去重后的结果到文件
 output_dir = "result"  # 修改保存目录
+os.makedirs(output_dir, exist_ok=True)  # 创建保存目录（如果不存在）
 output_file = os.path.join(output_dir, "rest.txt")  # 修改保存文件名
 
 with open(output_file, 'w') as file:
