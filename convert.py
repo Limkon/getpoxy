03e2e504cad7d16876e7033dbeafa05ad45d3206
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-10 12:55:53 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ import base64
 data_dir = "data"  # 替换为你的数据文件所在目录
 data_files = os.listdir(data_dir)
 
-merged_content = set()
+merged_content = []
 
 # 遍历数据文件，逐个检测内容并处理
 for file in data_files:
@@ -20,7 +20,7 @@ for file in data_files:
             try:
                 # 尝试解密 Base64 编码的内容
                 decoded_content = base64.b64decode(content).decode()
-                merged_content.add(decoded_content)
+                merged_content.append(decoded_content)
             except Exception as e:
                 # 内容不是 Base64 编码，跳过该文件并打印错误信息
                 print(f"Error processing file {file}: {str(e)}")
@@ -28,7 +28,7 @@ for file in data_files:
         else:
             print(f"Warning: Unknown file type for file {file}")
 
-# 输出合并且去重后的结果到文件
+# 保存合并后的内容到文件
 output_dir = "result"  # 修改保存目录
 os.makedirs(output_dir, exist_ok=True)  # 创建保存目录（如果不存在）
 output_file = os.path.join(output_dir, "rest.txt")  # 修改保存文件名
@@ -36,3 +36,18 @@ output_file = os.path.join(output_dir, "rest.txt")  # 修改保存文件名
 with open(output_file, 'w') as file:
     for data in merged_content:
         file.write(data + '\n')
+
+# 去除重复的行
+unique_lines = set()
+final_content = []
+with open(output_file, 'r') as file:
+    for line in file:
+        line = line.strip()
+        if line not in unique_lines:
+            final_content.append(line)
+            unique_lines.add(line)
+
+# 保存去重后的内容到文件
+with open(output_file, 'w') as file:
+    for line in final_content:
+        file.write(line + '\n')
