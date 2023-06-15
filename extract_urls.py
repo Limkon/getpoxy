59aa36141e582b00615a06f178728f2828cdffa6
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-15 23:15:03 +0800

    Update extract_urls.py

--- extract_urls.py
+++ extract_urls.py
@@ -1,12 +1,24 @@
 import requests
+import random
 from bs4 import BeautifulSoup
-import os
 
-def extract_subscription_urls(search_query):
-    search_url = f"https://www.google.com/search?q={search_query}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+def extract_subscription_urls(search_query, search_engine):
+    if search_engine == "google":
+        search_url = f"https://www.google.com/search?q={search_query}"
+    elif search_engine == "bing":
+        search_url = f"https://www.bing.com/search?q={search_query}"
+    else:
+        raise ValueError("Invalid search engine")
+
+    # 随机选择User-Agent标头
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36"
+    ]
+    user_agent = random.choice(user_agents)
+    headers = {"User-Agent": user_agent}
 
     response = requests.get(search_url, headers=headers)
     response.raise_for_status()
@@ -22,14 +34,24 @@ def extract_subscription_urls(search_query):
 
     return subscription_urls
 
-# 从环境变量中获取搜索关键字
+# 从环境变量中获取搜索关键字和搜索引擎
 search_query = os.getenv("SEARCH_QUERY", "订阅节点")
-urls = extract_subscription_urls(search_query)
+search_engine = os.getenv("SEARCH_ENGINE", "google")
 
-# 打印提取到的订阅地址
-for url in urls:
-    print(url)
+# 使用Google搜索并随机调整请求头信息
+if search_engine == "google":
+    urls = extract_subscription_urls(search_query, "google")
+    print("Google Search Results:")
+    for url in urls:
+        print(url)
 
-# 将提取到的订阅地址追加保存到 "furls" 文件中
+# 使用必应搜索并随机调整请求头信息
+elif search_engine == "bing":
+    urls = extract_subscription_urls(search_query, "bing")
+    print("Bing Search Results:")
+    for url in urls:
+        print(url)
+
+# 保存提取到的订阅地址到文件中
 with open("furls", "w") as file:
     file.write("\n".join(urls) + "\n")
