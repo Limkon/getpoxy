c406699e23e4f386d340a909ece2aa92acd804c7
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-21 14:25:47 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -71,25 +71,22 @@ for line in final_content:
         print(f"Error connecting to node {node_ip}:{node_port}: {str(e)}")
         break
 
-if tested_content:
-    # 保存测试后的内容到文件
-    tested_output_file = os.path.join(output_dir, "tested_rest.txt")  # 修改保存文件名
-    with open(tested_output_file, 'w') as file:
-        for line in tested_content:
-            file.write(line + '\n')
+# 保存测试后的内容到文件
+tested_output_file = os.path.join(output_dir, "tested_rest.txt")  # 修改保存文件名
+with open(tested_output_file, 'w') as file:
+    for line in tested_content:
+        file.write(line + '\n')
 
-    # 将最终结果 tested_rest.txt 使用 BASE64 编码后保存到 share/tongyy 文件中
-    output_share_dir = "share"  # 修改分享目录
-    os.makedirs(output_share_dir, exist_ok=True)  # 创建分享目录（如果不存在）
-    output_share_file = os.path.join(output_share_dir, "tongyy")  # 修改保存文件名
+# 将最终结果 tested_rest.txt 使用 BASE64 编码后保存到 share/tongyy 文件中
+output_share_dir = "share"  # 修改分享目录
+os.makedirs(output_share_dir, exist_ok=True)  # 创建分享目录（如果不存在）
+output_share_file = os.path.join(output_share_dir, "tongyy")  # 修改保存文件名
 
-    # 将最终内容进行 BASE64 编码
-    encoded_content = base64.b64encode('\n'.join(tested_content).encode()).decode()
+# 将最终内容进行 BASE64 编码
+encoded_content = base64.b64encode('\n'.join(tested_content).encode()).decode()
 
-    # 保存编码后的内容到文件
-    with open(output_share_file, 'w') as file:
-        file.write(encoded_content)
+# 保存编码后的内容到文件
+with open(output_share_file, 'w') as file:
+    file.write(encoded_content)
 
-    print(f"连接测试失败，将原结果内容进行 BASE64 编码保存至文件: {output_share_file}")
-else:
-    print("连接测试失败，无测试结果可保存。")
+print(f"连接测试失败，将原结果内容进行 BASE64 编码保存至文件: {output_share_file}")
