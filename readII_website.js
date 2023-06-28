3b99e92e141eb7e7d4e707b3d1245b08bf11e7f7
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-28 21:19:09 +0800

    Add files via upload

--- readII_website.js
+++ readII_website.js
@@ -2,10 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const moment = require('moment');
 const puppeteer = require('puppeteer-core');
-const PQueue = require('p-queue');
-const limit = new PQueue({ concurrency: concurrencyLimit });
-
-const concurrencyLimit = 15; // 同时处理的最大请求数
 
 (async () => {
   try {
@@ -13,80 +9,78 @@ const concurrencyLimit = 15; // 同时处理的最大请求数
       executablePath: 'google-chrome-stable',
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
+    const page = await browser.newPage();
 
+    // 将页面等待时间更改为 5000 毫秒
+    page.setDefaultTimeout(5000);
+
+    // 读取文件内容，获取所有要抓取的 URL 列表
     const urls = fs
-      .readFileSync('urls', 'utf-8')
+      .readFileSync('furls', 'utf-8')
       .split('\n')
       .map(url => url.trim())
       .filter(url => url !== '');
 
-    const limiter = limit(concurrencyLimit);
+    for (const url of urls) {
+      try {
+        await page.goto(url);
 
-    await Promise.all(urls.map(async (url) => {
-      await limiter(async () => {
-        try {
-          const page = await browser.newPage();
-          // 设置页面的默认超时时间为2秒
-          page.setDefaultTimeout(2000);
+        // 尝试不同的选择器
+        const selectors = [
+          '#app',                 // ID 选择器        
+        ];
 
-          await page.goto(url);
+        let content = '';
+        let success = false;
 
-          const selectors = [
-            '#app',                 // ID 选择器        
-          ];
-
-          let content = '';
-          let success = false;
-
-          for (const selector of selectors) {
-            try {
-              // 等待页面中的特定元素出现，超时时间设置为3秒
-              await page.waitForSelector(selector, { timeout: 3000 });
-              const element = await page.$(selector);
-              content = await page.evaluate(element => element.innerText, element);
-              success = true;
-              break;
-            } catch (error) {
-              console.error(`尝试通过选择器 ${selector} 获取 ${url} 内容失败：${error.message}`);
-            }
+        for (const selector of selectors) {
+          try {
+            await page.waitForSelector(selector);
+            const element = await page.$(selector);
+            content = await page.evaluate(element => element.innerText, element);
+            success = true;
+            break;
+          } catch (error) {
+            console.error(`尝试通过选择器 ${selector} 获取 ${url} 内容失败：${error.message}`);
           }
-
-          if (!success) {
-            console.error(`所有选择器都无法获取 ${url} 的内容，将执行自定义代码`);
-
-            const customContent = await page.evaluate(() => {
-              // 在此编写自定义的 JavaScript 代码来选择和提取页面内容
-              // 例如：返回整个页面的 innerText
-              return document.documentElement.innerText;
-            });
-
-            if (customContent) {
-              content = customContent;
-              console.log(`通过自定义代码成功提取了 ${url} 的内容`);
-            } else {
-              console.error(`自定义代码也无法获取 ${url} 的内容`);
-              return;
-            }
-          }
-
-          const date = moment().format('YYYY-MM-DD');
-          const urlWithoutProtocol = url.replace(/^(https?:\/\/)/, '');
-          const fileName = path.join('data', `${urlWithoutProtocol.replace(/[:?<>|"*\r\n/]/g, '_')}_${date}.txt`);
-
-          fs.writeFileSync(fileName, content);
-
-          console.log(`网站 ${url} 内容已保存至文件：${fileName}`);
-
-          await page.close();
-        } catch (error) {
-          console.error(`处理 ${url} 失败：${error.message}`);
         }
-      });
-    }));
 
+        // 如果所有选择器都失败，则执行自定义 JavaScript 代码提取页面内容
+        if (!success) {
+          console.error(`所有选择器都无法获取 ${url} 的内容，将执行自定义代码`);
+
+          const customContent = await page.evaluate(() => {
+            // 在此编写自定义的 JavaScript 代码来选择和提取页面内容
+            // 例如：返回整个页面的 innerText
+            return document.documentElement.innerText;
+          });
+
+          if (customContent) {
+            content = customContent;
+            console.log(`通过自定义代码成功提取了 ${url} 的内容`);
+          } else {
+            console.error(`自定义代码也无法获取 ${url} 的内容`);
+            continue;
+          }
+        }
+
+        const date = moment().format('YYYY-MM-DD');
+        const urlWithoutProtocol = url.replace(/^(https?:\/\/)/, '');
+        const fileName = path.join('bata', `${urlWithoutProtocol.replace(/[:?<>|"*\r\n/]/g, '_')}_${date}.txt`);
+
+        fs.writeFileSync(fileName, content);
+
+        console.log(`网站 ${url} 内容已保存至文件：${fileName}`);
+      } catch (error) {
+        console.error(`处理 ${url} 失败：${error.message}`);
+      }
+    }
+
+    await browser.close();
     console.log('所有网站内容保存完成！');
 
-    const directoryPath = 'data';
+    // 提取链接并追加到文件中
+    const directoryPath = 'bata';
     const urlsFilePath = 'urls';
 
     fs.readdir(directoryPath, (err, files) => {
@@ -117,8 +111,6 @@ const concurrencyLimit = 15; // 同时处理的最大请求数
         console.log('没有找到链接需要追加到文件。');
       }
     });
-
-    await browser.close();
   } catch (error) {
     console.error(error);
     process.exit(1);
