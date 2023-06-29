0afcb9dff44b51e5b6f86289481b5e43153bf7ea
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-29 23:11:24 +0800

    Update read_website.js

--- read_website.js
+++ read_website.js
@@ -12,26 +12,25 @@ const yaml = require('js-yaml');
     });
     const page = await browser.newPage();
 
-    // 将页面等待时间更改为 5000 毫秒
+    // Set the page timeout to 5000 milliseconds
     page.setDefaultTimeout(5000);
 
-    // 读取文件内容，获取所有要抓取的 URL 列表
+    // Read the file to get the list of URLs to scrape
     const urls = fs
       .readFileSync('urls', 'utf-8')
       .split('\n')
       .map(url => url.trim())
       .filter(url => url !== '');
 
-    const preservedFiles = []; // 用于存储保留的文件
-    const preservedUrls = []; // 用于存储保留的 URL
+    const preservedFiles = []; // Store the preserved files
+    const preservedUrls = []; // Store the preserved URLs
 
     for (const url of urls) {
       try {
         await page.goto(url);
 
-        // 尝试不同的选择器
         const selectors = [
-          '#app',                 // ID 选择器
+          '#app', // ID selector
         ];
 
         let content = '';
@@ -45,104 +44,91 @@ const yaml = require('js-yaml');
             success = true;
             break;
           } catch (error) {
-            console.error(`尝试通过选择器 ${selector} 获取 ${url} 内容失败：${error.message}`);
+            console.error(`Failed to retrieve content from ${url} using selector ${selector}: ${error.message}`);
           }
         }
 
-        // 如果所有选择器都失败，则执行自定义 JavaScript 代码提取页面内容
         if (!success) {
-          console.error(`所有选择器都无法获取 ${url} 的内容，将执行自定义代码`);
+          console.error(`Unable to retrieve content from ${url} using any of the selectors. Executing custom code.`);
 
           const customContent = await page.evaluate(() => {
-            // 在此编写自定义的 JavaScript 代码来选择和提取页面内容
-            // 例如：返回整个页面的 innerText
+            // Write custom JavaScript code here to select and extract page content
+            // For example: return the innerText of the entire page
             return document.documentElement.innerText;
           });
 
           if (customContent) {
             content = customContent;
-            console.log(`通过自定义代码成功提取了 ${url} 的内容`);
+            console.log(`Successfully extracted content from ${url} using custom code.`);
           } else {
-            console.error(`自定义代码也无法获取 ${url} 的内容`);
+            console.error(`Custom code failed to retrieve content from ${url}.`);
             continue;
           }
         }
 
-        // 检测内容是否为 BASE64 编码或特定格式
         if (!isBase64(content) && !isSpecialFormat(content)) {
-          // 检测是否为 JSON 文件
           let isJsonFile = false;
           try {
             const jsonContent = JSON.parse(content);
             if (jsonContent && typeof jsonContent === 'object') {
               isJsonFile = true;
-              // 在此处理 JSON 文件，例如保存到指定目录
               const date = moment().format('YYYY-MM-DD');
-              const urlWithoutProtocol = url.replace(/^(https?:\/\/)/, '');
-              const fileName = path.join('json_files', `${urlWithoutProtocol.replace(/[:?<>|"*\r\n/]/g, '_')}_${date}.json`);
+              const fileName = path.join('json_files', `${sanitizeFileName(url)}_${date}.json`);
               fs.writeFileSync(fileName, content);
-              console.log(`网站 ${url} 内容是 JSON 文件，已保存至文件：${fileName}`);
+              console.log(`Website ${url} content is a JSON file, saved to: ${fileName}`);
 
-              // 保留文件和 URL
               preservedFiles.push(fileName);
               preservedUrls.push(url);
             }
           } catch (error) {
-            // 不是有效的 JSON 文件
+            // Not a valid JSON file
           }
 
-          // 检测是否为 YAML 文件
           let isYamlFile = false;
           try {
             const yamlContent = yaml.safeLoad(content);
             if (yamlContent && typeof yamlContent === 'object') {
               isYamlFile = true;
-              // 在此处理 YAML 文件，例如保存到指定目录
               const date = moment().format('YYYY-MM-DD');
-              const urlWithoutProtocol = url.replace(/^(https?:\/\/)/, '');
-              const fileName = path.join('yaml_files', `${urlWithoutProtocol.replace(/[:?<>|"*\r\n/]/g, '_')}_${date}.yaml`);
+              const fileName = path.join('yaml_files', `${sanitizeFileName(url)}_${date}.yaml`);
               fs.writeFileSync(fileName, content);
-              console.log(`网站 ${url} 内容是 YAML 文件，已保存至文件：${fileName}`);
+              console.log(`Website ${url} content is a YAML file, saved to: ${fileName}`);
 
-              // 保留文件和 URL
               preservedFiles.push(fileName);
               preservedUrls.push(url);
             }
           } catch (error) {
-            // 不是有效的 YAML 文件
+            // Not a valid YAML file
           }
 
-          // 如果内容不是 BASE64 编码、特定格式、JSON 文件或 YAML 文件，则跳过处理
           if (!isJsonFile && !isYamlFile) {
             const urlIndex = preservedUrls.indexOf(url);
             if (urlIndex !== -1) {
               preservedUrls.splice(urlIndex, 1);
             }
-            console.error(`获取的 ${url} 内容既不是 BASE64 编码也不符合特定格式，将从 URL 列表中删除`);
+            console.error(`The content retrieved from ${url} is neither BASE64 encoded nor in a special format. Skipping.`);
             continue;
           }
         }
 
         const date = moment().format('YYYY-MM-DD');
-        const urlWithoutProtocol = url.replace(/^(https?:\/\/)/, '');
-        const fileName = path.join('data', `${urlWithoutProtocol.replace(/[:?<>|"*\r\n/]/g, '_')}_${date}.txt`);
+        const fileName = path.join('data', `${sanitizeFileName(url)}_${date}.txt`);
 
         fs.writeFileSync(fileName, content);
 
         preservedFiles.push(fileName);
         preservedUrls.push(url);
-        console.log(`网站 ${url} 内容已保存至文件：${fileName}`);
+        console.log(`Website ${url} content saved to file: ${fileName}`);
       } catch (error) {
-        console.error(`处理 ${url} 失败：${error.message}`);
+        console.error(`Failed to process ${url}: ${error.message}`);
       }
     }
 
-    // 更新 URL 列表文件，将保留的 URL 写回文件
     fs.writeFileSync('urls', preservedUrls.join('\n'));
-    console.log('更新后的 URL 列表已保存到文件！');
+    console.log('Updated URL list saved to file!');
 
     await browser.close();
-    console.log('所有网站内容保存完成！');
+    console.log('All website contents saved!');
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -157,4 +143,9 @@ function isBase64(str) {
 function isSpecialFormat(str) {
   const specialFormatRegex = /vmess:\/\/|trojan:\/\/|clash:\/\/|ss:\/\/|vlss:\/\//;
   return specialFormatRegex.test(str);
+}
+
+function sanitizeFileName(url) {
+  const urlWithoutProtocol = url.replace(/^(https?:\/\/)/, '');
+  return urlWithoutProtocol.replace(/[:?<>|"*\r\n/]/g, '_');
 }
